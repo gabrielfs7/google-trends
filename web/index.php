125bdcba92e30ea93561d81ca6656d56694d2089
gabrielfs7 <gabrielfs7@gmail.com>
2020-03-22 12:00:39 +0100

    chore: Make index example work only with JSON

--- web/index.php
+++ web/index.php
@@ -1,48 +1,27 @@
-<?php require __DIR__ . '/../vendor/autoload.php'; ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Google Trends - Analysis</title>
-</head>
-<body>
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use GSoares\GoogleTrends\Search;
+use GSoares\GoogleTrends\Category;
+
+header('Content-Type', 'application/json');
+
 try {
-    $results = (new GSoares\GoogleTrends\Search())
-        ->setCategory(GSoares\GoogleTrends\Category::BEAUTY_AND_FITNESS)
-        ->setLocation('US')
-        ->setLanguage('en-US')
-        ->addWord(isset($_GET['word']) ? $_GET['word'] : 'hair')
-        ->setLastDays(90)
-        ->searchRelatedTerms();
-    ?>
-    <h1>Google Trends - Results</h1>
-    <table border="1">
-        <thead>
-        <th>TOTAL</th>
-        <th colspan="2">URL</th>
-        </thead>
-        <tbody>
-        <td><?= count($results->getResults()) ?></td>
-        <td colspan="2"><a href="<?= $results->getSearchUrl() ?>"><?= $results->getSearchUrl() ?></a></td>
-        </tbody>
-        <thead>
-        <th>Term</th>
-        <th>Ranking</th>
-        <th>Search URL</th>
-        </thead>
-        <tbody>
-        <?php foreach ($results->getResults() as $term) { ?>
-        <td><?= $term->getTerm() ?></td>
-        <td><?= $term->getRanking() ?>%</td>
-        <td><a href="<?= $term->getSearchUrl() ?>"><?php echo $term->getSearchUrl() ?></a></td>
-        </tbody>
-        <?php } ?>
-    </table>
-    <?php
-} catch (\Exception $e) {
-    echo '<h1>Google Trends - ERROR</h1>';
-    echo $e->getMessage();
+    $search = new Search();
+    $search->getQueryBuilder()
+        ->withToken($_GET['token'] ?? '')
+        ->withCategory($_GET['category'] ?? 44) //Beauty & Fitness
+        ->withWord($_GET['word'] ?? 'hair')
+        ->withLastDays(90)
+        ->withTopMetrics()
+        ->withRisingMetrics();
+
+    echo json_encode($search->searchRelatedTerms()->jsonSerialize());
+} catch (Throwable $exception) {
+    echo json_encode(
+        [
+            'exception' => get_class($exception),
+            'error' => $exception->getMessage()
+        ]
+    );
 }
-?>
-</body>
-</html>
